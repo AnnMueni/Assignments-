c2284de1410559be88d0462f569704343c25a7e1
Mueni Matheka <annmueni0111@gmail.com>
2025-03-24 20:25:10 +0300

    Add files via upload

---
+++ one_dimension_array.c
@@ -0,0 +1,24 @@
+/*
+Author:Mueni Matheka
+Date:24.03.2025
+one_dimension_array
+*/
+#include<stdio.h>
+
+int main()
+{
+ int i;
+ int marks [5]={50,60,70,80,90};
+ //for(start;stop;step)
+ for(i=0;i<5;i++){
+ printf("%d\n",marks[i]);
+ }
+ /*
+ printf("%d\n",marks[0]);
+ printf("%d\n",marks[1]);
+ printf("%d\n",marks[2]);
+ printf("%d\n",marks[3]);
+ printf("%d\n",marks[4]);
+ */
+ return 0;
+ }
---
+++ three_dimension_array.c
@@ -0,0 +1,32 @@
+/*
+Author:Mueni Matheka
+Date:24.03.2025
+three_dimension_array
+*/
+
+#include<stdio.h>
+int main()
+{
+int i,j,k;
+int marks[2][2][3]={
+{
+ {1,2,3},
+ {4,5,6}
+},
+{
+ {10,20,30},
+ {40,50,60}
+}
+};
+ //for(start;stop;step)
+ for(i=0;i<2;i++){
+ for(j=0;j<2;j++){
+ for(k=0;k<3;k++){
+ printf("%d\t",marks[i][j][k]);
+ }
+ printf("\n");
+ }
+ printf("\n");
+ }
+ return 0;
+ }
